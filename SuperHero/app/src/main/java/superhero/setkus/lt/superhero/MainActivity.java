e45486a92b84f3ab11fc9b2142d9ecee8b65cb4a
Robertas Setkus <robertas.setkus@gmail.com>
2018-06-07 00:40:41 +0100

    Initial commit

---
+++ SuperHero/app/src/main/java/superhero/setkus/lt/superhero/MainActivity.java
@@ -0,0 +1,13 @@
+package superhero.setkus.lt.superhero;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
