d7a9ee0d52d8ba008b8f0f26afae70f331798186
Robertas Setkus <robertas.setkus@gmail.com>
2018-08-02 19:54:43 +0100

    Converted all to Kotlin.

---
+++ SuperHero/app/src/main/kotlin/lt/setkus/superhero/MainActivity.kt
@@ -0,0 +1,13 @@
+package lt.setkus.superhero
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import superhero.setkus.lt.superhero.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ SuperHero/app/src/test/kotlin/lt/setkus/superhero/ExampleTest.kt
@@ -0,0 +1,11 @@
+package lt.setkus.superhero
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class ExampleTest {
+    @Test
+    fun `testExample`() {
+        assertEquals("test", "test")
+    }
+}
