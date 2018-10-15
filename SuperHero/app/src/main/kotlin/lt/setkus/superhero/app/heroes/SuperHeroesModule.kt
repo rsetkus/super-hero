0b75e361f8cd73a466ac28e96da76bbf7cd13ebc
Robertas Setkus <robertas.setkus@gmail.com>
2018-10-15 20:56:39 +0100

    Removed Koin ViewModel library which caused compile error.

--- SuperHero/app/src/main/kotlin/lt/setkus/superhero/app/heroes/SuperHeroesModule.kt
+++ SuperHero/app/src/main/kotlin/lt/setkus/superhero/app/heroes/SuperHeroesModule.kt
@@ -1,10 +1,8 @@
 package lt.setkus.superhero.app.heroes
 
 import lt.setkus.superhero.data.heroes.SuperHeroesDataRepository
-import org.koin.android.viewmodel.ext.koin.viewModel
 import org.koin.dsl.module.module
 
 val superHeroesModule = module {
-    viewModel { SuperHeroesViewModel(get(name = "uiContext"), get(name = "networkContext"), get()) }
     single { SuperHeroesDataRepository(get()) }
 }
