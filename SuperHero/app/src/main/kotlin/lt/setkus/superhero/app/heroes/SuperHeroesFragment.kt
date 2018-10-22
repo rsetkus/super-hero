8a5bb293ff58be3d9cb0ec0bb410a8574855749b
Robertas Setkus <robertas.setkus@gmail.com>
2018-10-22 23:42:18 +0100

    RecyclerView dependency added. Also, it was added into layout and binded in fragment class.

--- SuperHero/app/src/main/kotlin/lt/setkus/superhero/app/heroes/SuperHeroesFragment.kt
+++ SuperHero/app/src/main/kotlin/lt/setkus/superhero/app/heroes/SuperHeroesFragment.kt
@@ -6,25 +6,27 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.RecyclerView
 import lt.setkus.superhero.R
 import lt.setkus.superhero.app.common.ViewState
 import org.koin.android.ext.android.inject
-import timber.log.Timber
 
 class SuperHeroesFragment : Fragment() {
+
+    private lateinit var grid: RecyclerView
 
     private val viewModel: SuperHeroesViewModel by inject()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        Timber.d("View has been created.")
-        return inflater.inflate(R.layout.super_heroes_fragment, container, false)
+        val rootView = inflater.inflate(R.layout.super_heroes_fragment, container, false)
+        grid = rootView.findViewById(R.id.grid)
+
+        return rootView
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        Timber.d("onCreate has been called")
-        Timber.d(viewModel.toString())
         val observer = Observer<ViewState> { viewState ->
             println(viewState)
         }
