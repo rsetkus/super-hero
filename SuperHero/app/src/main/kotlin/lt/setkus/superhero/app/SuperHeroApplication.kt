f41e76b11059c87b573277ec07a80f8a48ecdd1c
Robertas Setkus <robertas.setkus@gmail.com>
2018-11-25 23:03:19 +0000

    LeakCanary library added.

--- SuperHero/app/src/main/kotlin/lt/setkus/superhero/app/SuperHeroApplication.kt
+++ SuperHero/app/src/main/kotlin/lt/setkus/superhero/app/SuperHeroApplication.kt
@@ -1,6 +1,7 @@
 package lt.setkus.superhero.app
 
 import android.app.Application
+import com.squareup.leakcanary.LeakCanary
 import lt.setkus.superhero.BuildConfig
 import lt.setkus.superhero.app.di.module.httpModule
 import lt.setkus.superhero.app.heroes.superHeroesModule
@@ -11,6 +12,13 @@ class SuperHeroApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return
+        }
+
+        LeakCanary.install(this)
 
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
